cdbb6943e12f244765b5d5998c2046573b8e5074
ch-canaza <ch-canaza@hotmail.com>
2020-06-27 17:57:59 -0500

    addition of kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,22 @@ class BaseModel:
     """
         that defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             initializes BaseModel class
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
