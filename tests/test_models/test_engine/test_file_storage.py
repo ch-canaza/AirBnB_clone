efd65969890573c505dd0e6e10fd019e1d3b787c
Javier <ingenierodesoftware73@gmail.com>
2020-06-29 21:40:59 +0000

    fixing tests_file_storeage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,12 @@
 import models
-from models.engine.file_storage import file_storage
+from models.engine.file_storage import FileStorage
+import os
 import unittest
+"""from models.base_model import BaseModel"""
+"""import json"""
 
 
-class testFile_Storage(unittest.TestCase):
+class TestFile_Storage(unittest.TestCase):
     """we start the test"""
     
     
@@ -16,4 +19,19 @@ class testFile_Storage(unittest.TestCase):
         msj = "Classes doesnt have docstring"
         self.assertIsNotNone(obj, msj) # Classes
         
-    def test_executable_file(self)
+    def test_executable_file(self):
+        '''test if file has permissions u+x to executable'''
+        #Check for read access
+        is_read_true = os.access('models/engine/file_storage.py', os.R_OK)
+        self.assertTrue(is_read_true)
+        #check for write access
+        is_write_true = os.access('models/engine/file_storage.py', os.W_OK)
+        self.assertTrue(is_write_true)
+        #Check for execution access
+        is_exec_true = os.access('models/engine/file_storage.py', os.X_OK)
+        self.assertTrue(is_exec_true)
+        
+    def test_is_an_instance(self):
+        '''check if my_models is an instance of BaseModel'''
+        my_model = FileStorage()
+        self.assertIsInstance(my_model, FileStorage)
