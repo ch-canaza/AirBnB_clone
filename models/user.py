58dcd40dafaf2bbac38406c60d012446b0ba5786
Javier <ingenierodesoftware73@gmail.com>
2020-06-29 22:48:55 +0000

    create a user class

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+user class
+"""
+
+
+from models.base_model import BaseModel
+
+
+
+
+class User(BaseModel):
+    """Class to create user"""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
