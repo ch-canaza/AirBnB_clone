06f9fd78fde6ec4094c8c16d5aa0b88ad3d988df
Javier <ingenierodesoftware73@gmail.com>
2020-07-01 19:32:30 +0000

    the hope

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,20 @@
 """
     file that initializes the file storage
 """
-from models.engine.file_storage import FileStorage
 
+
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+all_classes = {"User": User, "BaseModel": BaseModel,
+               "Place": Place, "State": State,
+               "City": City, "Amenity": Amenity,
+               "Review": Review}
 storage = FileStorage()
 storage.reload()
